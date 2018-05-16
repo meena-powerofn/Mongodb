553affc89ee7f8302ab0d04f118b8cf39086b8a8
meena <meena@powerofn.in>
2018-05-16 13:07:10 +0530

    Mongo installation completed

--- config_ubuntu.sh
+++ config_ubuntu.sh
@@ -7,22 +7,15 @@ echo '\033[0;31m'"Mongodb Installation 3.6 started"'\033[0m'
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5 &&
 
-echo "deb http://repo.mongodb.org/apt/debian jessie/mongodb-org/3.6 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list &&
+echo 'deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list' &&
 
-sudo apt-get update 
+sudo apt-get update -y &&
 
 sudo apt-get install -y mongodb-org=3.6.4 mongodb-org-server=3.6.4 mongodb-org-shell=3.6.4 mongodb-org-mongos=3.6.4 mongodb-org-tools=3.6.4 &&
 
-#sudo chmod -R 777 /var/log/mongodb
-
-#sudo chmod -R 777 /var/lib/mongodb
-
+sudo service mongod start &&
 
 sudo systemctl enable mongod &&
-
-
-sudo service mongod start  &&
-
 
 echo '\033[0;32m'"Installation Completed"'\033[0m'
 
